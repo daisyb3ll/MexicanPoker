0729bb92a375ed868a602a1838c34e6b03669f98
daisyb3ll <daisy.fernandez.zr@gmail.com>
2024-03-04 15:55:35 -0800

    yay!
    jdensd

---
+++ poker.py
@@ -0,0 +1,25 @@
+import random
+from collections import Counter
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import List, Tuple
+
+class Suit(enum):
+    SPADES = "♠"
+    HEARTS = "♥"
+    DIAMONDS = "♦"
+    CLUBS = "♣"
+class Rank(enum):
+    Two = 2
+    Three =3
+    Four =4
+    Five = 5
+    Six = 6
+    Seven = 7
+    Eight = 8
+    Nine = 9
+    Ten = 10 
+    JACK = 11
+    QUEEN = 12 
+    KING = 12 
+    ACE = 13 
